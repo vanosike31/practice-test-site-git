df9c008401f892a75e695e0cf2227c0e78e63b50
Victor Anosike <vanosike31@gmail.com>
2017-08-10 13:11:53 -0400

    Added username change functionality

--- scripts/main.js
+++ scripts/main.js
@@ -12,8 +12,44 @@ myImage.onclick = function() {
 	}
 }
 
+
 //variable myImage is attatched to the .onclick event, which allows it to listen for a click to the varable myImage
 //myImage.onclick is set to a FUNCTION THAT INITIATES WHEN THE PICTURE IS CLICKED
 //This fuction sets the variable mySrc to the .getAttribute function of variable myImage, which recieves the 
 //src attribute of the original myImage element
 //Conditional states that if mySrc is set to the varia suit samus src clicking the src is changed to zss, and vice versa
+
+var myHeading = document.querySelector('h1');
+var myButton = document.querySelector('button');
+
+//Defines the variables myHeading and myButtion as functions that pull and return the first instances of the selected elements
+
+function setusername () {
+	var myName = prompt('Please enter name ');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Meet Samus, ' + myName; 
+}
+
+//defines setusername as a function that does mutliple things
+//prompt is a function that creates a prompt for creating info
+//myName is equal to the answer that you type into the prompt
+//localStorage.setItem function is will memorize a data item 'name' as the variable myName
+//myHeading's textcontent property is set to the written string
+
+if (!localStorage.getItem('name')) {
+	setusername();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Meet Samus, ' + storedName;
+}
+
+
+//conditional that allows a new username to be set if one doesn't already exist
+//! means not available; if the 'name' data item isnt in local storage, than the seyusername function is activated
+//if it is, the variable storedName takes info from the localstorage datatype set as name, and then set as heading
+
+myButton.onclick = function() {
+	setusername();
+}
+
+//function setusername is activated via the onclick event
